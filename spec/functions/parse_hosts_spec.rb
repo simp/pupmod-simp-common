aa05b12bcbeed4b7767b7e529156504eb8f4d11f
nick-markowski <nmarkowski@keywcorp.com>
2015-06-29 18:47:30 +0000

    (SIMP-30) Create a parse_hosts function.
    Created a function to parse host strings/urls. Returns
    a hash of hostnames with an array of ports and an array of protocols.
    Modified strip_ports to use parse_hosts and added get_ports
    to return stripped ports.
    
    SIMP-30 #close Create a parse_hosts function.
    SIMP-66 #close Created assets to help pupmod-simp-common run
    Change-Id: Ifab2f52cec98b8e4eb1109d5ff9e72947abdf921

---
+++ spec/functions/parse_hosts_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'parse_hosts' do
+  # IPv4
+  it do
+    retval = scope.function_parse_hosts(["http://some.domain.net",
+                                         "http://localhost.my.domain:8989",
+                                         "http://localhost.my.domain:8989",
+                                         "fuu.bar.baz",
+                                         "my.example.net:900",
+                                         "my.example.net:700"])
+    expect(retval).to eq({"some.domain.net"=>{:ports=>[], :protocol=>["http"]},
+                          "localhost.my.domain"=>{:ports=>["8989"], :protocol=>["http"]},
+                          "fuu.bar.baz"=>{:ports=>[], :protocol=>[]},
+                          "my.example.net"=>{:ports=>["900", "700"], :protocol=>[]}})
+  end
+  # IPv6
+  it do
+    retval = scope.function_parse_hosts(["http://[2001:db8:1f70::999:de8:7648:6e8]:100",
+                                         "http://[2001:db8:1f70::999:de8:7648:6e8]:100",
+                                         "http://[2001:dc8:1f70::999:de8:7648:6e8]",
+                                         "[2001:dd8:1f70::bbb:de8:7648:6e8]",
+                                         "[2001:de8:1f70::999:de8:7648:6e8]:100",
+                                         "http://2001:db8:1f70::999:de8:7648:bbb",
+                                         "2001:db8:1f70::999:de8:7648:ccc"])
+    expect(retval).to eq({"2001:db8:1f70::999:de8:7648:6e8"=>{:ports=>["100"], :protocol=>["http"]},
+                          "2001:dc8:1f70::999:de8:7648:6e8"=>{:ports=>[], :protocol=>["http"]},
+                          "2001:dd8:1f70::bbb:de8:7648:6e8"=>{:ports=>[], :protocol=>[]},
+                          "2001:de8:1f70::999:de8:7648:6e8"=>{:ports=>["100"], :protocol=>[]},
+                          "2001:db8:1f70::999:de8:7648:bbb"=>{:ports=>[], :protocol=>["http"]},
+                          "2001:db8:1f70::999:de8:7648:ccc"=>{:ports=>[], :protocol=>[]}})
+  end
+end
