64f826ac71039aad149234e5956a13ae2a1b94fb
Trevor Vaughan <tvaughan@onyxpoint.com>
2015-10-29 21:42:03 -0400

    (SIMP-561) Fixed issues with secure_mountpoints
    There were quite a few issues with the way the secure_mountpoints class
    was working.
    
    This really should be made into a custom type in the future.
    
    SIMP-584 #close
    SIMP-564 #comment Code Fixes
    
    Change-Id: I196ef5a4360eb8b2d92e2a67a1a95027fbf6fe10

--- spec/classes/secure_mountpoints_spec.rb
+++ spec/classes/secure_mountpoints_spec.rb
@@ -1,101 +1,91 @@
 require 'spec_helper'
 
 describe 'common::secure_mountpoints' do
-  base_facts = {
-    :operatingsystem => 'CentOS',
-    :operatingsystemmajrelease => '6',
-    :operatingsystemrelease => '6.5',
-    :ipaddress => '10.10.10.10',
-    :fqdn => 'foo.bar.baz',
-    :hostname => 'foo',
-    :interfaces => 'eth0',
-    :ipaddress_eth0 => '10.10.10.10',
-    :selinux_current_mode => 'enforcing'
-  }
+  on_supported_os.each do |os, base_facts|
 
-  let(:facts){base_facts}
+    context "on #{os}" do
+      let(:facts){ base_facts.dup }
+      it { should compile.with_all_deps }
+      it { should contain_mount('/dev/pts').with_options('rw,gid=5,mode=620,noexec') }
+      it { should contain_mount('/sys').with_options('rw,nodev,noexec') }
+      it { should contain_mount('/tmp').with({
+        :options => 'bind,nodev,noexec,nosuid',
+        :device  => '/tmp'
+      })}
+      it { should contain_mount('/var/tmp').with({
+        :options => 'bind,nodev,noexec,nosuid',
+        :device  => '/tmp'
+      })}
 
-  it {
-    should compile.with_all_deps
-  }
-  it { should contain_mount('/dev/pts').with_options('rw,gid=5,mode=620,noexec') }
-  it { should contain_mount('/sys').with_options('rw,nodev,noexec') }
-  it { should contain_mount('/tmp').with({
-    :options => 'bind,defcontext=system_u:object_r:tmp_t:s0,nodev,noexec,nosuid',
-    :device  => '/tmp'
-  })}
-  it { should contain_mount('/var/tmp').with({
-    :options => 'bind,defcontext=system_u:object_r:tmp_t:s0,nodev,noexec,nosuid',
-    :device  => '/tmp'
-  })}
+      context 'tmp_is_partition' do
+        new_facts = base_facts.dup
+        new_facts[:tmp_mount_tmp] = 'rw,seclabel,relatime,data=ordered'
+        new_facts[:tmp_mount_fstype_tmp] = 'ext4'
+        new_facts[:tmp_mount_path_tmp] = '/dev/sda3'
 
-  context 'tmp_is_partition' do
-    new_facts = base_facts.dup
-    new_facts[:tmp_mount_tmp] = 'rw,seclabel,relatime,data=ordered'
-    new_facts[:tmp_mount_fstype_tmp] = 'ext4'
-    new_facts[:tmp_mount_path_tmp] = '/dev/sda3'
+        let(:facts){new_facts}
 
-    let(:facts){new_facts}
+        it { should contain_mount('/tmp').with({
+          :options => "#{new_facts[:tmp_mount_tmp]},noexec,nosuid,nodev".split(',').sort.join(','),
+          :device  => '/dev/sda3'
+        })}
+      end
 
-    it { should contain_mount('/tmp').with({
-      :options => "#{new_facts[:tmp_mount_tmp]},noexec,nosuid,nodev".split(',').sort.join(','),
-      :device  => '/dev/sda3'
-    })}
-  end
+      context 'tmp_is_already_bind_mounted' do
+        new_facts = base_facts.dup
+        new_facts[:tmp_mount_tmp] = 'bind,foo'
+        new_facts[:tmp_mount_fstype_tmp] = 'ext4'
+        new_facts[:tmp_mount_path_tmp] = '/tmp'
 
-  context 'tmp_is_already_bind_mounted' do
-    new_facts = base_facts.dup
-    new_facts[:tmp_mount_tmp] = 'bind,foo'
-    new_facts[:tmp_mount_fstype_tmp] = 'ext4'
-    new_facts[:tmp_mount_path_tmp] = '/tmp'
+        let(:facts){facts.merge(new_facts)}
 
-    let(:facts){new_facts}
+        it { should contain_mount('/tmp').with({
+          :options => "bind,nodev,noexec,nosuid",
+          :device  => '/tmp'
+        })}
+      end
 
-    it { should contain_mount('/tmp').with({
-      :options => "bind,defcontext=system_u:object_r:tmp_t:s0,nodev,noexec,nosuid",
-      :device  => '/tmp'
-    })}
-  end
+      context 'var_tmp_is_partition' do
+        new_facts = base_facts.dup
+        new_facts[:tmp_mount_var_tmp] = 'rw,seclabel,relatime,data=ordered'
+        new_facts[:tmp_mount_fstype_var_tmp] = 'ext4'
+        new_facts[:tmp_mount_path_var_tmp] = '/dev/sda3'
 
-  context 'var_tmp_is_partition' do
-    new_facts = base_facts.dup
-    new_facts[:tmp_mount_var_tmp] = 'rw,seclabel,relatime,data=ordered'
-    new_facts[:tmp_mount_fstype_var_tmp] = 'ext4'
-    new_facts[:tmp_mount_path_var_tmp] = '/dev/sda3'
+        let(:facts){new_facts}
 
-    let(:facts){new_facts}
+        it { should contain_mount('/var/tmp').with({
+          :options => "#{new_facts[:tmp_mount_var_tmp]},noexec,nosuid,nodev".split(',').sort.join(','),
+          :device  => '/dev/sda3'
+        })}
+      end
 
-    it { should contain_mount('/var/tmp').with({
-      :options => "#{new_facts[:tmp_mount_var_tmp]},noexec,nosuid,nodev".split(',').sort.join(','),
-      :device  => '/dev/sda3'
-    })}
-  end
+      context 'var_tmp_is_already_bind_mounted' do
+        new_facts = base_facts.dup
+        new_facts[:tmp_mount_var_tmp] = 'bind,foo'
+        new_facts[:tmp_mount_fstype_var_tmp] = 'ext4'
+        new_facts[:tmp_mount_path_var_tmp] = '/var/tmp'
 
-  context 'var_tmp_is_already_bind_mounted' do
-    new_facts = base_facts.dup
-    new_facts[:tmp_mount_var_tmp] = 'bind,foo'
-    new_facts[:tmp_mount_fstype_var_tmp] = 'ext4'
-    new_facts[:tmp_mount_path_var_tmp] = '/var/tmp'
+        let(:facts){new_facts}
 
-    let(:facts){new_facts}
+        it { should contain_mount('/var/tmp').with({
+          :options => "bind,nodev,noexec,nosuid",
+          :device  => new_facts[:tmp_mount_path_var_tmp]
+        })}
+      end
 
-    it { should contain_mount('/var/tmp').with({
-      :options => "bind,defcontext=system_u:object_r:tmp_t:s0,nodev,noexec,nosuid",
-      :device  => new_facts[:tmp_mount_path_var_tmp]
-    })}
-  end
+      context 'tmp_mount_dev_shm_mounted' do
+        new_facts = base_facts.dup
+        new_facts[:tmp_mount_dev_shm] = 'rw,seclabel,nosuid,nodev'
+        new_facts[:tmp_mount_fstype_dev_shm] = 'tmpfs'
+        new_facts[:tmp_mount_path_dev_shm] = 'tmpfs'
 
-  context 'tmp_mount_dev_shm_mounted' do
-    new_facts = base_facts.dup
-    new_facts[:tmp_mount_dev_shm] = 'rw,seclabel,nosuid,nodev'
-    new_facts[:tmp_mount_fstype_dev_shm] = 'tmpfs'
-    new_facts[:tmp_mount_path_dev_shm] = 'tmpfs'
+        let(:facts){new_facts}
 
-    let(:facts){new_facts}
-
-    it { should contain_mount('/dev/shm').with({
-      :options => 'nodev,noexec,nosuid,rw,seclabel',
-      :device  => new_facts[:tmp_mount_path_dev_shm]
-    })}
+        it { should contain_mount('/dev/shm').with({
+          :options => 'nodev,noexec,nosuid,rw,seclabel',
+          :device  => new_facts[:tmp_mount_path_dev_shm]
+        })}
+      end
+    end
   end
 end
